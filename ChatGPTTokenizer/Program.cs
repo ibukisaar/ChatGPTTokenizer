4bc5fceb925024e7c42d3e1ce31c74852e91c9fd
Saar Ibuki <rockman.x8@qq.com>
2023-02-12 17:44:20 +0800

    更新 Program.cs

--- ChatGPTTokenizer/Program.cs
+++ ChatGPTTokenizer/Program.cs
@@ -5,5 +5,5 @@ string text = """
     print("Hello world!")
     """;
 var tokens = tokenizer.Encode(text);
-Console.WriteLine($"count: {tokens.Length}");
-Console.WriteLine(string.Join(',', tokens.Select(t => t.Id)));
+Console.WriteLine($"count: {tokens.Length}"); // count: 6
+Console.WriteLine(string.Join(',', tokens.Select(t => t.Id))); // 4798,7203,15496,995,2474,8
